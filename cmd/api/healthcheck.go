d4349fc0a77adfb488620458dbb712efa57fc2e7
Michael Pacleb <michael.pacleb@gmail.com>
2024-06-10 08:19:25 -0700

    updated healthcheckHandler to use json.Marshal to encode data to JSON via new writeJSON helper method.

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,18 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// Create a fixed-format JSON response from a string.
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	// Create a map which holds the information that we want to send in the response.
+	data := map[string]string{
+		"status": "available",
+		"environment": app.config.env,
+		"version": version,
+	}
 
-	// Set the "Content-Type" header on the response.
-	w.Header().Set("Content-Type", "application/json")
-
-	// Write the JSON as the HTTP response body.
-	w.Write([]byte(js))
+	// Pass the map to the json.Marshal() function. This returns a []byte slice containing the encoded JSON.
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered an error and could not process your request", http.StatusInternalServerError)
+	}
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -22,4 +23,32 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+
+// Helper method for sending JSON responses. It takes the destination ResponseWriter, HTTP status code to send, 
+// the data to encode to JSON, and header map containing HTTP headers to set.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	// Encode the data to JSON by passing to the json.Marshal() function. This returns a []byte slice containing the encoded JSON.
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Append newline to the JSON to make it easier to view in terminal apps.
+	js = append(js, '\n')
+
+	// Loop through the headers map and add each to the response header.
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	// Set HTTP header 'Content-Type' as 'application/json' and write the status code and response.
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	// Send the []byte slice containing the JSON as response body.
+	w.Write(js)
+
+	return nil
 }
