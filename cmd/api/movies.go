309c802a57f034365936b39538221871509b72a9
Michael Pacleb <michael.pacleb@gmail.com>
2024-06-10 21:15:02 -0700

    created a custom Movie struct to represent a single Movie in the system and updated showMovieHandler to encode this Movie struct to JSON using writeJSON helper.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/micypac/flick-info/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -18,5 +21,20 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "Showing the details of move %d\n", id)
+	// Create new instance of Movie struct, containing the ID we extracted from URL parameter and some dummy data.
+	movie := data.Movie{
+		ID: id,
+		CreatedAt: time.Now(),
+		Title: "Casablanca",
+		Runtime: 102,
+		Genres: []string{"drama", "romance", "war"},
+		Version: 1,
+	}
+
+	// Encode the struct to JSON and send it as the HTTP response.
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
---
+++ internal/data/movies.go
@@ -0,0 +1,13 @@
+package data
+
+import "time"
+
+type Movie struct {
+	ID 				int64				`json:"id"`									// Unique integer id for the movie.
+	CreatedAt time.Time		`json:"-"`									// Timestamp when the movie is added to the db. '-' struct tag directive to hide in the output.
+	Title 		string			`json:"title"`		
+	Year 			int32				`json:"year,omitempty"`			// Release year. 'omitempty' struct directive to hide field in the output if the it is zero value.
+	Runtime 	int32				`json:"runtime,omitempty"`	// Runtime (in minutes).
+	Genres 		[]string		`json:"genres,omitempty"`		// Genres of the movie.
+	Version 	int32				`json:"version"`						// Version starts at 1 and incremented when movie info is updated.
+}
