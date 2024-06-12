80b054f7391f543be86f8fbb5dbb2a12b3dcad59
Michael Pacleb <michael.pacleb@gmail.com>
2024-06-11 22:07:23 -0700

    4.5 - validating JSON input by creating Validator struct with helper methods and functions to make sure JSON request fields meets our business rules before processing.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/micypac/flick-info/internal/data"
+	"github.com/micypac/flick-info/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +22,22 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from input struct to new Movie struct.
+	movie := &data.Movie{
+		Title: input.Title,
+		Year: input.Year,
+		Runtime: input.Runtime,
+		Genres: input.Genres,
+	}
+
+	// Initialize a new Validator instance.
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
---
+++ internal/validator/validator.go
@@ -0,0 +1,71 @@
+package validator
+
+import (
+	"regexp"
+)
+
+var (
+	EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+)
+
+type Validator struct {
+	Errors map[string]string
+}
+
+
+// Helper function to create new Validator instance with empty Errors map.
+func New() *Validator {
+	return &Validator{Errors: make(map[string]string)}
+}
+
+
+// Valid return true if Errors map is empty.
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+
+// AddError adds an error message to the map if entry does not exist.
+func (v *Validator) AddError(key, message string) {
+	if _, exist := v.Errors[key]; !exist {
+		v.Errors[key] = message
+	}
+}
+
+
+// Adds an error message to the map only if a validation check is not 'ok'.
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+
+// Returns true if 'value' is in the 'list'.
+func In(value string, list ...string) bool {
+	for i := range list {
+		if value == list[i] {
+			return true
+		}
+	}
+
+	return false
+}
+
+
+// Returns true if a string value matches a specific regexp pattern.
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+
+// Returns true if all the items in the slice are unique.
+func Unique(values []string) bool {
+	uniqueValues := make(map[string]bool)
+
+	for _, value := range values {
+		uniqueValues[value] = true
+	}
+
+	return len(values) == len(uniqueValues)
+}
