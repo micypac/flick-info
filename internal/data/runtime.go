25aca4f87c7fc39b22f7d7a0c53b43f4d0ca721c
Michael Pacleb <michael.pacleb@gmail.com>
2024-06-11 21:10:32 -0700

    4.4 - custom JSON decoding by implementing Runtime type UnmarshalJSON() method. 'runtime' JSON field will be received with '<runtime> mins' valueand will decoded into Runtime int32 type.

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,9 +1,14 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+// Custom error UnmarshalJSON() can return if parsing JSON failed.
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 // Declare a custom Runtime type, which has underlying type int32.
 // This is used as a field in Movie struct and to customize the output format using the MarshalJSON method.
@@ -20,4 +25,28 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+// Implement UnmarshalJSON() method on the Runtime type so it satisfies the json.Unmarshaler interface.
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(i)
+
+	return nil
 }
