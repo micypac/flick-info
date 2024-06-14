292ecb0a1a5fd72813673bdc090d32b3c5cadf3c
Michael Pacleb <michael.pacleb@gmail.com>
2024-06-14 14:56:04 -0700

    7.4 - implemented updating a movie. Created new endpoint PUT /v1/movies/:id, MovieModel Update() method, and created updatedMovieHandler.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -89,4 +89,66 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 }
 
 
+func(app *application) updateMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	// Fetch the existing movie record from the db.
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Declare an input struct to hold the expected data from the client.
+	var input struct{
+		Title string `json:"title"`
+		Year int32 `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres []string `json:genres`
+	}
+
+	// Read JSON request body into the input struct.
+	err = app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the request body to the appropriate fields of the movie record.
+	movie.Title = input.Title
+	movie.Year = input.Year
+	movie.Runtime = input.Runtime
+	movie.Genres = input.Genres
+
+	// Validate the updated movie record.
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Pass the updated movie record to the Update() method.
+	err = app.models.Movies.Update(movie)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+
 
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -100,7 +100,22 @@ func (m MovieModel) Get(id int64) (*Movie, error) {
 
 
 func (m MovieModel) Update(movie *Movie) error {
-	return nil
+	stmt := `
+		UPDATE movies 
+		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
+		WHERE id = $5
+		RETURNING version
+	`
+
+	args := []interface{}{
+		movie.Title,
+		movie.Year,
+		movie.Runtime,
+		pq.Array(movie.Genres),
+		movie.ID,
+	}
+
+	return m.DB.QueryRow(stmt, args...).Scan(&movie.Version)
 }
 
 
