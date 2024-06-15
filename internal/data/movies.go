5563a3a74e72e40ae7e23498d27d9d2c3b985253
Michael Pacleb <michael.pacleb@gmail.com>
2024-06-14 21:56:07 -0700

    8.2 - implemented optimistic concurrency control to prevent data race during movie update.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -103,7 +103,7 @@ func (m MovieModel) Update(movie *Movie) error {
 	stmt := `
 		UPDATE movies 
 		SET title = $1, year = $2, runtime = $3, genres = $4, version = version + 1
-		WHERE id = $5
+		WHERE id = $5 AND version = $6
 		RETURNING version
 	`
 
@@ -113,9 +113,20 @@ func (m MovieModel) Update(movie *Movie) error {
 		movie.Runtime,
 		pq.Array(movie.Genres),
 		movie.ID,
+		movie.Version,
 	}
 
-	return m.DB.QueryRow(stmt, args...).Scan(&movie.Version)
+	err := m.DB.QueryRow(stmt, args...).Scan(&movie.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
 
 
