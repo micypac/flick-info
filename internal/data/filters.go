d55d1e3838ddf68a1acb6f1775b6dd59b7382ab0
Michael Pacleb <michael.pacleb@gmail.com>
2024-06-16 19:20:13 -0700

    9.6 - GET /v1/movies endpoint can now supports how movies are sorted in the JSON response.

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,10 @@
 package data
 
-import "github.com/micypac/flick-info/internal/validator"
+import (
+	"strings"
+
+	"github.com/micypac/flick-info/internal/validator"
+)
 
 type Filters struct {
 	Page int
@@ -17,3 +21,28 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 
 	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
 }
+
+
+// Check that the client provided Sort field matches any one of the entries in our safelist.
+// If it does, return the column name by stripping the leading '-' character if it has one.
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter:" + f.Sort)
+}
+
+
+// Return the sort direction depending on the prefix of the Sort field.
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
+}
+
+
