2d1bd3c661cbe3173c72f00eade8f8e5d864f019
Michael Pacleb <michael.pacleb@gmail.com>
2024-06-18 07:41:04 -0700

    10.2 - created recoverPanic() middleware to send 'internal server error' to client rather than closing the HTTP connection with no context.

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,7 +6,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func (app *application) routes() *httprouter.Router {
+func (app *application) routes() http.Handler {
 	// Initialize a new httprouter.Router instance.
 	router := httprouter.New()
 
@@ -26,5 +26,6 @@ func (app *application) routes() *httprouter.Router {
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 
-	return router
+	// Wrap the router with the panic recover middleware.
+	return app.recoverPanic(router)
 }
