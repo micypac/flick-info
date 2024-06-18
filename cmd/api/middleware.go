3cfdf1aa30b3a0b4e447db4c183fcdb3f59ead1c
Michael Pacleb <michael.pacleb@gmail.com>
2024-06-18 09:10:53 -0700

    11.1 - added a global rate limiter feature that allows an avg of 2 request/sec with a max of 4 requests in a single burst. This is done via rateLimit() middleware that wraps the entire router.

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -14,6 +16,25 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 				app.serverErrorResponse(w, r, fmt.Errorf("%s", err))
 			}
 		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+
+//
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	// Init new rate limiter which allows an average of 2req/sec, with a max of 4req in a single 'bursts'.
+	limiter := rate.NewLimiter(2, 4)
+
+	// Return a function closure which 'closes' over the limiter verbiage.
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Call limiter.Allow() to see if the request is permitted. 
+		// If its not, return a 429 Too Many Requests response.
+		if !limiter.Allow() {
+			app.rateLimitExceedResponse(w, r)
+			return
+		}
 
 		next.ServeHTTP(w, r)
 	})
