3230b3432f7e127e5b75e2eeb4158a2273dd4c2f
Michael Pacleb <michael.pacleb@gmail.com>
2024-06-27 07:30:03 -0700

    14.4 - fixed sending email to be in a background goroutine to reduce latency.

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -58,14 +58,18 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	}
 
 
-	// Call the Send() method on the Mailer, passing in the user's email address,
-	// name of the template file, and the User struct containing the dynamic data.
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl.html", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+	// Use the background() helper to execute an anonymous function that sends the welcome email.
+	app.background(func() {
+		// Call the Send() method on the Mailer, passing in the user's email address,
+		// name of the template file, and the User struct containing the dynamic data.
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl.html", user)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+		}
+	
+	})
 
+	
 	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
