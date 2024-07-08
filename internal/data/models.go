38f4e3ddde1986961ad969ba683accfb29b69147
Michael Pacleb <michael.pacleb@gmail.com>
2024-07-08 14:04:40 -0700

    17.3 - created PermissionModel and GetAllForUser() method to get all permissions like 'read', 'write', etc. for a specific user.

--- internal/data/models.go
+++ internal/data/models.go
@@ -13,6 +13,7 @@ var (
 
 type Models struct {
 	Movies MovieModel
+	Permissions PermissionModel
 	Tokens TokenModel
 	Users UserModel
 }
@@ -21,6 +22,7 @@ type Models struct {
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Permissions: PermissionModel{DB: db},
 		Tokens: TokenModel{DB: db},
 		Users: UserModel{DB: db},
 	}
